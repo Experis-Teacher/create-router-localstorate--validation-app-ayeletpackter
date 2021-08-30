a9845b55767b07c95b5a8a82128fa2e629c1d2b8
ayeletpackter <ayeletpackter@gmail.com>
2021-08-30 12:57:40 +0300

    adding validation to the name

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,21 +3,30 @@ import React, { Component } from "react";
 export default class About extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      invalidName:''
+    };
   }
 
   saveName(e) {
+    console.log("gggggggg")
     localStorage["name"] = e.target.value;
   }
 
   navigate = () => {
+    const regex = /[A-Z]/i;
+    if(regex.test(localStorage.getItem("name")))
     this.props.history.push("/contact");
+else
+this.setState({invalidName:"invalid name"})
+      
   }
 
   render() {
     return (
       <div>
         <h1> About</h1>
+        {this.state.invalidName}
         <input
           type="text"
           placeholder="Enter your name"
